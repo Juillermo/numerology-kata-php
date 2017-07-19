01419588a4f0b49f6797114928c958c5e969dd15
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-20 00:23:34 +0200

    change tests to use all numbers to ensure test independence

--- tests/Numerology/ReplacerTest.php
+++ tests/Numerology/ReplacerTest.php
@@ -8,32 +8,35 @@ use Numerology\ReplacementGenerator\TwoTens;
 
 class ReplacerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDoesNotReplaceOnes()
+    public function testDoesNotReplace()
     {
         $replacer = new Replacer();
         self::assertEquals([1], $replacer->replace([1]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesNineWithTwoTens()
     {
         $replacer = new Replacer();
         $replacer->registerReplacementGenerator(9, new TwoTens());
-        self::assertEquals([10,10], $replacer->replace([9]));
-        self::assertEquals([1,3,4,5,7,8,10,10,10], $replacer->replace([1,3,4,5,7,8,9,10]));
+        self::assertEquals([10, 10], $replacer->replace([9]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesTwosWithOnes()
     {
         $replacer = new Replacer();
         $replacer->registerReplacementGenerator(2, new AnEqualAmountOfOnesAsTheNumberToTheLeft());
-        self::assertEquals([1,1], $replacer->replace([1,2]));
-        self::assertEquals([3,1,1,1], $replacer->replace([3,2]));
+        self::assertEquals([1, 1], $replacer->replace([1, 2]));
+        self::assertEquals([1, 1, 3, 4, 5, 6, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
+
     public function testReplacesSixesWithThrees()
     {
         $replacer = new Replacer();
-        $replacer->registerReplacementGenerator(6, new AnEqualAmountOfThreesAsTheNumberAnAmountOfStepsToTheRightAsTheNumberWhichIsToTheImmediateLeft());
-        self::assertEquals([3,3,5,1,1], $replacer->replace([3,6,5,1,1]));
-        self::assertEquals([3,3,3,3,5,1,3], $replacer->replace([3,6,5,1,3]));
+        $replacer->registerReplacementGenerator(6,
+            new AnEqualAmountOfThreesAsTheNumberAnAmountOfStepsToTheRightAsTheNumberWhichIsToTheImmediateLeft());
+        self::assertEquals([1, 3 ,1], $replacer->replace([1, 6, 1]));
+        self::assertEquals([1, 2, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
     }
 }
