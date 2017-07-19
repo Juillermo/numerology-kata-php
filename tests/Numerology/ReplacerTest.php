78ac7a919680721b676839929fcfb62d40ee4cdd
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-20 00:30:31 +0200

    use 'setUp' to reduce some duplicity

--- tests/Numerology/ReplacerTest.php
+++ tests/Numerology/ReplacerTest.php
@@ -8,35 +8,40 @@ use Numerology\ReplacementGenerator\TwoTens;
 
 class ReplacerTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var Replacer */
+    private $replacer;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->replacer = new Replacer();
+    }
+
     public function testDoesNotReplace()
     {
-        $replacer = new Replacer();
-        self::assertEquals([1], $replacer->replace([1]));
-        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        self::assertEquals([1], $this->replacer->replace([1]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesNineWithTwoTens()
     {
-        $replacer = new Replacer();
-        $replacer->registerReplacementGenerator(9, new TwoTens());
-        self::assertEquals([10, 10], $replacer->replace([9]));
-        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        $this->replacer->registerReplacementGenerator(9, new TwoTens());
+        self::assertEquals([10, 10], $this->replacer->replace([9]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesTwosWithOnes()
     {
-        $replacer = new Replacer();
-        $replacer->registerReplacementGenerator(2, new AnEqualAmountOfOnesAsTheNumberToTheLeft());
-        self::assertEquals([1, 1], $replacer->replace([1, 2]));
-        self::assertEquals([1, 1, 3, 4, 5, 6, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        $this->replacer->registerReplacementGenerator(2, new AnEqualAmountOfOnesAsTheNumberToTheLeft());
+        self::assertEquals([1, 1], $this->replacer->replace([1, 2]));
+        self::assertEquals([1, 1, 3, 4, 5, 6, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesSixesWithThrees()
     {
-        $replacer = new Replacer();
-        $replacer->registerReplacementGenerator(6,
+        $this->replacer->registerReplacementGenerator(6,
             new AnEqualAmountOfThreesAsTheNumberAnAmountOfStepsToTheRightAsTheNumberWhichIsToTheImmediateLeft());
-        self::assertEquals([1, 3 ,1], $replacer->replace([1, 6, 1]));
-        self::assertEquals([1, 2, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 7, 8, 9, 10], $replacer->replace([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
+        self::assertEquals([1, 3 ,1], $this->replacer->replace([1, 6, 1]));
+        self::assertEquals([1, 2, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
     }
 }
