f53dea6d52723329e934d108c81eba259865d9eb
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-20 00:31:20 +0200

    reformat lines too long to improve readability

--- tests/Numerology/ReplacerTest.php
+++ tests/Numerology/ReplacerTest.php
@@ -20,28 +20,32 @@ class ReplacerTest extends \PHPUnit_Framework_TestCase
     public function testDoesNotReplace()
     {
         self::assertEquals([1], $this->replacer->replace([1]));
-        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesNineWithTwoTens()
     {
         $this->replacer->registerReplacementGenerator(9, new TwoTens());
         self::assertEquals([10, 10], $this->replacer->replace([9]));
-        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10],
+            $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesTwosWithOnes()
     {
         $this->replacer->registerReplacementGenerator(2, new AnEqualAmountOfOnesAsTheNumberToTheLeft());
         self::assertEquals([1, 1], $this->replacer->replace([1, 2]));
-        self::assertEquals([1, 1, 3, 4, 5, 6, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+        self::assertEquals([1, 1, 3, 4, 5, 6, 7, 8, 9, 10],
+            $this->replacer->replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
     }
 
     public function testReplacesSixesWithThrees()
     {
         $this->replacer->registerReplacementGenerator(6,
             new AnEqualAmountOfThreesAsTheNumberAnAmountOfStepsToTheRightAsTheNumberWhichIsToTheImmediateLeft());
-        self::assertEquals([1, 3 ,1], $this->replacer->replace([1, 6, 1]));
-        self::assertEquals([1, 2, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 7, 8, 9, 10], $this->replacer->replace([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
+        self::assertEquals([1, 3, 1], $this->replacer->replace([1, 6, 1]));
+        self::assertEquals([1, 2, 3, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 7, 8, 9, 10],
+            $this->replacer->replace([1, 2, 3, 5, 4, 6, 7, 8, 9, 10]));
     }
 }
